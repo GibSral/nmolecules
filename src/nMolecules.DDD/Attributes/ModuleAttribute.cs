abc7901815a3141865cec54b6fbc649d611fe6c2
Lars Baldes <lb@wps.de>
2022-01-23 21:53:03 +0100

    adjusted namespaces of attributes Factory and Module

--- src/nMolecules.DDD/Attributes/ModuleAttribute.cs
+++ src/nMolecules.DDD/Attributes/ModuleAttribute.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-namespace NMolecules.DDD.Attributes
+
+namespace NMolecules.DDD
 {
     /// <summary>
-    /// Identifies a DDD module.
-    /// 
-    /// <see href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
-    ///      Reference (Evans) - Modules</see>
+    ///     Identifies a DDD module.
+    ///     <see href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">
+    ///         Domain-Driven Design
+    ///         Reference (Evans) - Modules
+    ///     </see>
     /// </summary>
     [AttributeUsage(
-            AttributeTargets.Assembly |
-            AttributeTargets.Module)]
+        AttributeTargets.Assembly |
+        AttributeTargets.Module)]
     public class ModuleAttribute : Attribute
     {
     }
